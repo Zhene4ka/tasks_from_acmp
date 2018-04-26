317438264e9b3711bc057c2546aca100c02ad669
Zhene4ka <feldinazhenja@gmail.com>
2018-04-26 14:41:00 +0300

    fix task for negative numbers

--- sum.py
+++ sum.py
@@ -1,6 +1,12 @@
 num = None
+
 with open('input.txt', 'r') as input_file:
     num = int(input_file.readline())
 
 with open('output.txt', 'w') as output_file:
-    output_file.write(str(sum([i for i in range(num+1)])))
+    count = abs(num)
+    sum_for_positive_numbers = sum([i for i in range(count + 1)])
+    if num < 0:
+        output_file.write(str(int('-' + str(sum_for_positive_numbers)) + 1))
+    else:
+        output_file.write(str(sum_for_positive_numbers))
