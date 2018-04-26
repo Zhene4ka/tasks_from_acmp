7fa6228ecc7c7d93cd39c6477db152a3a36417bf
Zhene4ka <feldinazhenja@gmail.com>
2018-04-26 16:41:29 +0300

    correct task gold_of_ABBA_tribe

--- gold_of_ABBA_tribe.py
+++ gold_of_ABBA_tribe.py
@@ -1,6 +1,6 @@
 max_count_of_coins = None
 with open('input.txt', 'r') as input_file:
-    max_count_of_coins = max((input_file.readline().split(' ')))
+    max_count_of_coins = max(list(map(int, input_file.readline().split(' '))))
 
 with open('output.txt', 'w') as output_file:
     output_file.write(str(max_count_of_coins))
