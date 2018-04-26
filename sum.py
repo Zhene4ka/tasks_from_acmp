b6b543053fb31731dea177c57fbcd964dac60ff1
Zhene4ka <feldinazhenja@gmail.com>
2018-04-26 14:51:16 +0300

    fix for n=0

--- sum.py
+++ sum.py
@@ -6,7 +6,7 @@ with open('input.txt', 'r') as input_file:
 with open('output.txt', 'w') as output_file:
     count = abs(num)
     sum_for_positive_numbers = sum([i for i in range(count + 1)])
-    if num < 0:
-        output_file.write(str(int('-' + str(sum_for_positive_numbers)) + 1))
+    if num <= 0:
+        output_file.write(str(-sum_for_positive_numbers + 1))
     else:
         output_file.write(str(sum_for_positive_numbers))
