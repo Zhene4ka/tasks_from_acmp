fc6ccc2606222371377fa6045bf953489cc9ad57
Zhene4ka <feldinazhenja@gmail.com>
2018-04-26 16:35:17 +0300

    correct task statistic

--- statistic.py
+++ statistic.py
@@ -4,10 +4,10 @@ uneven_numbers_of_month = None
 mark = None
 with open('input.txt', 'r') as input_file:
     n = input_file.readline().strip()
-    numbers_of_month = list(map(int, input_file.readline().strip().split(" ")))
+    numbers_of_month = list(map(int, input_file.readline().split(" ")))
     even_numbers_of_month = list(filter(lambda n: n % 2 == 0, numbers_of_month))
     uneven_numbers_of_month = list(filter(lambda n: n % 2 != 0, numbers_of_month))
-    if len(uneven_numbers_of_month) < len(even_numbers_of_month):
+    if len(uneven_numbers_of_month) <= len(even_numbers_of_month):
         mark = 'YES'
     else:
         mark = 'NO'
